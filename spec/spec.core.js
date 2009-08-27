dc8489937b0f7824d18db8f3c9ef801a989609a9
visionmedia <tj@vision-media.ca>
2009-08-27 10:00:10 -0700

    clearInterval() returning bool

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -63,7 +63,7 @@ describe 'Timers'
       tick(200)
       called.should.be_true
       called = false
-      clearInterval(id)
+      clearInterval(id).should.be_true
       tick(400)
       called.should.be_false
     end
