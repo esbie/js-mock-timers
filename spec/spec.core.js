5c993bfd89cf2f2e161b2550472aa00853e5d5fc
visionmedia <tj@vision-media.ca>
2009-08-27 09:52:10 -0700

    Localized called in specs to prevent collision

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -1,75 +1,75 @@
 
 describe 'Timers'
-  // describe 'setTimeout()'
-  //   it 'should return unique ids'
-  //     id = setTimeout(function(){}, 200)
-  //     id2 = setTimeout(function(){}, 200)
-  //     id3 = setTimeout(function(){}, 200)
-  //     id2.should.eql id + 1
-  //     id3.should.eql id2 + 1
-  //   end
-  //   
-  //   it 'should not be called when ticked below the duration'
-  //     called = false
-  //     setTimeout(function(){ called = true }, 200)
-  //     tick(100)
-  //     called.should.be_false
-  //   end
-  //   
-  //   it 'should be called when ticked passed the duration'
-  //     called = false
-  //     setTimeout(function(){ called = true }, 200)
-  //     tick(200)
-  //     called.should.be_true
-  //   end
-  //   
-  //   it 'should be destroyed once called'
-  //     called = false
-  //     setTimeout(function(){ called = true }, 200)
-  //     tick(200)
-  //     called.should.be_true
-  //     called = false
-  //     tick(200)
-  //     called.should.be_false
-  //   end
-  // end
+  describe 'setTimeout()'
+    it 'should return unique ids'
+      id = setTimeout(function(){}, 200)
+      id2 = setTimeout(function(){}, 200)
+      id3 = setTimeout(function(){}, 200)
+      id2.should.eql id + 1
+      id3.should.eql id2 + 1
+    end
+    
+    it 'should not be called when ticked below the duration'
+      var called = false
+      setTimeout(function(){ called = true }, 200)
+      tick(100)
+      called.should.be_false
+    end
+    
+    it 'should be called when ticked passed the duration'
+      var called = false
+      setTimeout(function(){ called = true }, 200)
+      tick(200)
+      called.should.be_true
+    end
+    
+    it 'should be destroyed once called'
+      var called = false
+      setTimeout(function(){ called = true }, 200)
+      tick(200)
+      called.should.be_true
+      called = false
+      tick(200)
+      called.should.be_false
+    end
+  end
   
   describe 'setInterval()'
-    // it 'should return unique ids'
-    //   id = setInterval(function(){}, 200)
-    //   id2 = setInterval(function(){}, 200)
-    //   id3 = setInterval(function(){}, 200)
-    //   id2.should.eql id + 1
-    //   id3.should.eql id2 + 1
-    // end
-    // 
-    // it 'should be called when ticked passed the interval'
-    //   called = false
-    //   setInterval(function(){ called = true }, 200)
-    //   tick(100)
-    //   called.should.be_false
-    //   tick(100)
-    //   called.should.be_true
-    //   called = false
-    //   tick(100)
-    //   called.should.be_false
-    //   tick(200)
-    //   called.should.be_true
-    // end
-    // 
-    // it 'should be destroyed when id is passed to clearInterval()'
-    //   called = false
-    //   id = setInterval(function(){ called = true }, 200)
-    //   tick(200)
-    //   called.should.be_true
-    //   called = false
-    //   clearInterval(id)
-    //   tick(400)
-    //   called.should.be_false
-    // end
+    it 'should return unique ids'
+      id = setInterval(function(){}, 200)
+      id2 = setInterval(function(){}, 200)
+      id3 = setInterval(function(){}, 200)
+      id2.should.eql id + 1
+      id3.should.eql id2 + 1
+    end
+    
+    it 'should be called when ticked passed the interval'
+      var called = false
+      setInterval(function(){ called = true }, 200)
+      tick(100)
+      called.should.be_false
+      tick(100)
+      called.should.be_true
+      called = false
+      tick(100)
+      called.should.be_false
+      tick(200)
+      called.should.be_true
+    end
+    
+    it 'should be destroyed when id is passed to clearInterval()'
+      var called = false
+      id = setInterval(function(){ called = true }, 200)
+      tick(200)
+      called.should.be_true
+      called = false
+      clearInterval(id)
+      tick(400)
+      called.should.be_false
+    end
     
     it 'should be called several times when a multiple of the interval is ticked'
-      called = 0
+      var called = 0
       setInterval(function(){ ++called }, 200)
       called.should.eql 0
       tick(200)
